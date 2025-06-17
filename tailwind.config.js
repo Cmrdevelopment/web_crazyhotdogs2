7595f1d9cc55b33772180fd6bd842df2eef3002c
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2025-06-17 11:10:05 +0100

    fuenteen indez y tailwing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
-//   theme: {
-//     extend: {
-//       colors: {
-//         primaryrojo: "#C22634", 
-//         secondarynegro: "#000000",
-//         Mostaza: "#EAC60C",
-//         MostazaSuave: "#edd65f",
-//         footer: "#2E3E5C",
-//         cmrbolsa: "#328077",
-//         verdeClaro: "#31857E",
-//         gris: "#F3F4F6",
-//         fondosClaros: "#F2EFEC",
-//       },
-//     },
-//   },
-
-//   variants: {
-//     extend: {
-//       transitionProperty: [
-//         "hover",
-//         "focus",
-//         "responsive",
-//         "motion-safe",
-//         "motion-reduce",
-//       ],
-//     },
-//   },
-
-//   plugins: [],
-// };
-
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -51,6 +15,11 @@ export default {
         gris: "#F3F4F6",
         fondosClaros: "#F2EFEC",
       },
+
+      fontFamily: {
+        grobold: ['Grobold', 'sans-serif'], // ✅ AÑADIDO AQUÍ
+      },
+      
       animation: {
         ketchup: 'ketchupAnim 5s ease-in-out forwards',
       },
